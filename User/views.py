1ac3525e03c34b09fccd8ede07588d0bd1ebbf1f
Maruf512 <marufsarkar512@gmail.com>
2023-08-19 22:49:25 +0600

    got data on views from html

--- User/views.py
+++ User/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+
 def home(request):
-    return render(request, "login.html")
+    if(request.POST):
+        login_data = request.POST.dict()
+        username = login_data.get("input_user_name")
+        password = login_data.get("input_user_password")
+        print(username, password)
+        return HttpResponse("Login Successful.")
+    else:
+        return render(request, "login.html")
+
+
