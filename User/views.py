3378217a3eab2143e72b5ea01499975f973b6fb7
Maruf512 <marufsarkar512@gmail.com>
2023-08-20 19:13:45 +0600

    done login

--- User/views.py
+++ User/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
 
-
 def home(request):
     if(request.POST):
         login_data = request.POST.dict()
         username = login_data.get("input_user_name")
         password = login_data.get("input_user_password")
         print(username, password)
-        return HttpResponse("Login Successful.")
+        if username and password != "":
+            return HttpResponse("Login Successful.")
+        else:
+            return render(request, "login.html")
     else:
         return render(request, "login.html")
 
