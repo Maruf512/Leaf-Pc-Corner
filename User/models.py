b7c91d873ea589b46790a917c7f60587aa1ba6ca
Maruf512 <marufsarkar512@gmail.com>
2023-08-21 13:03:50 +0600

    done login

--- User/models.py
+++ User/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    u_name = models.CharField(max_length=50, null=False)
+    u_password = models.CharField(max_length=30, null=False)
--- User/views.py
+++ User/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render, HttpResponse
-
+from User.models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from django.contrib import messages
 
 def home(request):
     if(request.POST):
         login_data = request.POST.dict()
         username = login_data.get("input_user_name")
         password = login_data.get("input_user_password")
-        print(username, password)
-        if username and password != "":
-            return HttpResponse("Login Successful.")
+        
+        user = User.objects.filter(u_name=username)
+        print(user)
+        if user:
+            user_info = User.objects.filter(u_password=password)
+            if user_info:
+                return HttpResponse("Login Successful.")
+            else:
+                return render(request, "login.html")
         else:
             return render(request, "login.html")
     else:
+        # x = User.objects.get(id=1)
+        # print(x.u_name)
+        # print(x.u_password)
         return render(request, "login.html")
 
 
